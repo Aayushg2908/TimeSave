16b505c99029819fc40482ce04d751e09f3be953
Aayush <aayushg2908@gmail.com>
2024-07-17 22:36:57 +0530

    UI: basic layout

--- app/(landing)/navbar.tsx
+++ app/(landing)/navbar.tsx
@@ -1,12 +1,20 @@
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="h-[60px] w-full px-4 flex items-center justify-between">
-      <div className="flex items-center gap-x-1">
+      <Link href="/" className="flex items-center gap-x-1">
         <Image
           src="/logo.webp"
           alt="main-logo"
@@ -14,18 +22,28 @@ const Navbar = () => {
           height={50}
           className="size-12"
         />
-        <h1 className="font-semibold text-lg">TimeSave</h1>
-      </div>
-      <Link
-        href="/sign-in"
-        className={cn(
-          buttonVariants({
-            className: "font-bold",
-          })
-        )}
-      >
-        Login
+        <h1 className="font-semibold text-lg max-sm:hidden">TimeSave</h1>
       </Link>
+      <ClerkLoading>
+        <Loader2 className="size-8 animate-spin text-green-500" />
+      </ClerkLoading>
+      <ClerkLoaded>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <Link
+            href="/sign-in"
+            className={cn(
+              buttonVariants({
+                className: "font-bold",
+              })
+            )}
+          >
+            Login
+          </Link>
+        </SignedOut>
+      </ClerkLoaded>
     </nav>
   );
 };
---
+++ app/dashboard/[date]/page.tsx
@@ -0,0 +1,5 @@
+const MainPage = () => {
+  return <div>Home Page</div>;
+};
+
+export default MainPage;
