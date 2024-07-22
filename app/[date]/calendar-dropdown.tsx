cedfe7736760ef77b85b5c04d58d3d68a13d7b34
Aayush <aayushg2908@gmail.com>
2024-07-22 22:37:13 +0530

    feat: adding calendar

---
+++ app/[date]/calendar-dropdown.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Calendar } from "@/components/ui/calendar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const CalendarDropdown = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <span className="font-semibold">
+          {new Date().toLocaleDateString(undefined, {
+            month: "long",
+            day: "numeric",
+          })}
+        </span>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <Calendar
+          mode="single"
+          selected={new Date()}
+          onSelect={(date) => {
+            router.push(`/${date?.toISOString().split("T")[0]}`);
+          }}
+        />
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default CalendarDropdown;
--- app/[date]/navbar.tsx
+++ app/[date]/navbar.tsx
@@ -1,4 +1,5 @@
 import AccessibilityDropdown from "./accessibility-dropdown";
+import CalendarDropdown from "./calendar-dropdown";
 import UserDropdown from "./user-dropdown";
 
 const Navbar = () => {
@@ -6,7 +7,7 @@ const Navbar = () => {
     <nav className="z-10 fixed w-full bottom-3 flex flex-col items-center">
       <div className="h-[40px] w-[200px] rounded-full border shadow-lg flex items-center py-2 px-4 justify-between">
         <AccessibilityDropdown />
-        <div>Calendar</div>
+        <CalendarDropdown />
         <UserDropdown />
       </div>
     </nav>
